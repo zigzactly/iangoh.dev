1b031fe7129a238f45ce6927741d88a508ffb75c
Ian Goh <zigzactly@gmail.com>
2024-01-25 10:26:03 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <main className="bg-red-50 p-4">
+      <h1 className="font-bold">Hello, Next.js!</h1>
+    </main>
+  );
+}
